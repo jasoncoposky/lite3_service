9211b216c014c20e4b5fc78482f50108b8f59905
Jason M. Coposky <jason.coposky@gmail.com>
2026-01-29 18:19:41 -0500

    Repository cleanup: Removed logs, build artifacts, and non-essential files mistakenly committed. Updated .gitignore.

--- _deps/gtest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jason/playground/L3KV/_deps/gtest-src/googlemock
-# Build directory: C:/Users/jason/playground/L3KV/_deps/gtest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
